9438a533bddc7a4c75cb6ec44a9fa82a3971139e
calebtuttle <1calebtuttle@gmail.com>
2022-08-28 17:37:51 -0400

    Remove unused functions from src/main

--- src/main/utils/EnclaveInterface.js
+++
@@ -1,9 +0,0 @@
-/**
- * A wrapper around methods to interact with the secure enclave.
- */
-
-class EnclaveInterface {
-  static async startProof(creds, nullifier) {}
-}
-
-export { EnclaveInterface };
--- src/main/utils/dbWrapper.js
+++ src/main/utils/dbWrapper.js
@@ -1,5 +1,3 @@
-// const sqlite3 = require("sqlite3").verbose();
-// import sqlite3 from "sqlite3";
 import { sqlDb, jsonDb } from "../init.js";
 
 /**
