58728ae45a6e84901642a4826cd6eda62a9679bf
calebtuttle <1calebtuttle@gmail.com>
2022-09-07 17:13:45 -0400

    Start endpoint for proving knowledge of a preimage of a member leaf

--- src/main/routes/proofs.js
+++ src/main/routes/proofs.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { addSmallLeaf } from "../services/proofs.service.js";
+import {
+  addSmallLeaf,
+  proveKnowledgeOfPreimageOfMemberLeaf,
+} from "../services/proofs.service.js";
 
 const router = express.Router();
 
 router.get("/addSmallLeaf", addSmallLeaf);
+router.get(
+  "/proveKnowledgeOfPreimageOfMemberLeaf", // TODO: Better name
+  proveKnowledgeOfPreimageOfMemberLeaf
+);
 
 export default router;
--- src/main/services/proofs.service.js
+++ src/main/services/proofs.service.js
@@ -6,36 +6,6 @@ const exec = util.promisify(nonPromisifiedExec);
 const pyExecutable = process.env.PYTHON_EXECUTABLE;
 const enclaveClientScript = process.env.PATH_TO_ENCLAVE_CLIENT_SCRIPT;
 
-// /**
-//  * Takes an encrypted string containing two arguments: creds and secret.
-//  * Generates addSmallLeaf proof and proof that creds == "US".
-//  * The encrypted string is sent to the secure enclave where it is decrypted
-//  * and where the proofs are generated. The proofs are re-encrypted and returned
-//  * to the requestor.
-//  * @param {string} args Encrypted string. When decrypted, it is an object containing
-//  * creds and nullifier.
-//  */
-// export async function residenceProof(req, res) {
-//   console.log(`${new Date().toISOString()} residenceProof: Entered`);
-
-//   const args = req.query.args;
-//   if (!args) {
-//     return res.status(400).json({ error: `No arguments found in query string.` });
-//   }
-
-//   try {
-//     // command == python enclave_client.py generate-proofs [some_string]
-//     const { stdout, stderr } = await exec(
-//       `${pyExecutable} ${enclaveClientScript} generate-proofs ${args}`
-//     );
-
-//     return res.status(200).json({ data: stdout });
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(500).json({ error: "An unknown error occurred" });
-//   }
-// }
-
 /**
  * Generates addSmallLeaf proof.
  * The encrypted string is sent to the secure enclave where it is decrypted
@@ -55,7 +25,39 @@ export async function addSmallLeaf(req, res) {
   try {
     // command == python enclave_client.py generate-proofs [some_string]
     const { stdout, stderr } = await exec(
-      `${pyExecutable} ${enclaveClientScript} generate-proof addSmallLeaf ${args}`
+      `${pyExecutable} ${enclaveClientScript} generate-proof addSmallLeaf ${args} false`
+    );
+    const parsedStdout = JSON.parse(stdout);
+
+    return res.status(200).json({ data: parsedStdout });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "An unknown error occurred" });
+  }
+}
+
+/**
+ * Generates proveKnowledgeOfPreimageOfMemberLeaf proof.
+ * The encrypted string is sent to the secure enclave where it is decrypted
+ * and where the proof is generated. The proof is re-encrypted and returned
+ * to the requestor.
+ * @param {string} args Encrypted string. See the proof function for what params
+ * need to be present in the decrypted object.
+ */
+export async function proveKnowledgeOfPreimageOfMemberLeaf(req, res) {
+  console.log(
+    `${new Date().toISOString()} proveKnowledgeOfPreimageOfMemberLeaf: entered`
+  );
+
+  const { args, sharded } = req.query;
+  if (!args || !sharded) {
+    return res.status(400).json({ error: `No arguments found in query string.` });
+  }
+
+  try {
+    // command == python enclave_client.py generate-proofs [some_string]
+    const { stdout, stderr } = await exec(
+      `${pyExecutable} ${enclaveClientScript} generate-proof proveKnowledgeOfPreimageOfMemberLeaf ${args} ${sharded}`
     );
     const parsedStdout = JSON.parse(stdout);
 
