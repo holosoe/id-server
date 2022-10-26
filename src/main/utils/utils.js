dfa1cb344163b07cb4b2c18db27b578ee531d530
nanaknihal <nanaknihal@gmail.com>
2022-10-26 11:46:23 -0400

    updated test and utils

--- src/main/utils/utils.js
+++ src/main/utils/utils.js
@@ -1,4 +1,4 @@
-import { strict as assert } from "node:assert";
+import assert from "assert";
 import ethersPkg from "ethers";
 const { ethers } = ethersPkg;
 
@@ -20,8 +20,11 @@ export async function sign(data) {
 export function getDateAsInt(date) {
   // Format input
   const [year, month, day] = date.split("-");
-  assert.ok((year > 1900) && (year < 2099)); // Make sure date is in a reasonable range, otherwise it's likely the input was malformatted and it's best to be safe by stopping -- we can always allow more edge cases if needed later 
-  return (new Date(date)).getTime() / 1000 + 2208988800 // 2208988800000 is 70 year offset; Unix timestamps below 1970 are negative and we want to allow from approximately 1900. 
+  assert.ok(year && month && day); // Make sure Y M D all given
+  assert.ok((year >= 1900) && (year <= 2099)); // Make sure date is in a reasonable range, otherwise it's likely the input was malformatted and it's best to be safe by stopping -- we can always allow more edge cases if needed later 
+  const time = (new Date(date)).getTime() / 1000 + 2208988800 // 2208988800000 is 70 year offset; Unix timestamps below 1970 are negative and we want to allow from approximately 1900. 
+  assert.ok(!isNaN(time));
+  return time;
 }
 
 export function logWithTimestamp(message) {
--- test/test.js
+++ test/test.js
@@ -1,173 +1,89 @@
 import axios from "axios";
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { getDateAsBytes } from "../src/main/utils/utils.js";
+import { getDateAsInt } from "../src/main/utils/utils.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe("getDateAsBytes", async () => {
+describe("getDateAsInt", async () => {
   it("Should throw error if date is in format yyyy-mm", async () => {
     const date = "1900-01";
-    expect(() => getDateAsBytes(date)).to.throw();
+    expect(() => getDateAsInt(date)).to.throw();
   });
 
   it("Should throw error if date is in format dd-mm-yyyy", async () => {
     const date = "01-01-1900";
-    expect(() => getDateAsBytes(date)).to.throw();
+    expect(() => getDateAsInt(date)).to.throw();
   });
 
   it("Should throw error if date is in format mm-dd-yyyy", async () => {
     const date = "01-01-1900";
-    expect(() => getDateAsBytes(date)).to.throw();
+    expect(() => getDateAsInt(date)).to.throw();
   });
 
   it("Should throw error if date is in format yyyy/mm/dd", async () => {
     const date = "01/01/1900";
-    expect(() => getDateAsBytes(date)).to.throw();
+    expect(() => getDateAsInt(date)).to.throw();
   });
 
   // BEGIN 1900 tests
 
   it("Should throw an error, given 1900-01-32", async () => {
     const date = "1900-01-32";
-    expect(() => getDateAsBytes(date)).to.throw();
+    expect(() => getDateAsInt(date)).to.throw();
   });
 
-  it("Should convert 1900-01-01 to 0x000001", async () => {
+  it("Should convert 1900-01-01 to 0", async () => {
     const date = "1900-01-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x00, 0x00, 0x01]));
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(0);
   });
 
-  it("Should convert 1900-01-02 to 0x000002", async () => {
+  it("Should convert 1900-01-02 to 86400", async () => {
     const date = "1900-01-02";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x00, 0x00, 0x02]));
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(86400);
   });
 
-  it("Should convert 1900-01-31 to 0x00001f", async () => {
+  it("Should convert 1900-01-31 to 2592000", async () => {
     const date = "1900-01-31";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x00, 0x00, 0x1f]));
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(2592000);
   });
 
-  it("Should convert 1900-02-01 to 0x000020", async () => {
+  it("Should convert 1900-02-01 to 2678400", async () => {
     const date = "1900-02-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x00, 0x00, 0x20]));
-  });
-
-  it("Should convert 1900-02-28 to 0x00003b", async () => {
-    const date = "1900-02-28";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x00, 0x00, 0x3b]));
-  });
-
-  it("Should convert 1900-10-01 to 0x000112", async () => {
-    const date = "1900-10-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x00, 0x01, 0x12]));
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(2678400);
   });
 
   // BEGIN 1970 tests
 
   it("Should throw an error, given 1970-01-32", async () => {
     const date = "1970-01-32";
-    expect(() => getDateAsBytes(date)).to.throw();
+    expect(() => getDateAsInt(date)).to.throw();
   });
 
-  it("Should convert 1970-01-01 to 0x460001", async () => {
+  it("Should convert 1970-01-01 to 2208988800", async () => {
     const date = "1970-01-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x46, 0x00, 0x01]));
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(2208988800);
   });
 
-  it("Should convert 1970-01-02 to 0x460002", async () => {
+  it("Should convert 1970-01-02 to 2208988800+86400", async () => {
     const date = "1970-01-02";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x46, 0x00, 0x02]));
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(2208988800+86400);
   });
 
-  it("Should convert 1970-01-31 to 0x46001f", async () => {
-    const date = "1970-01-31";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x46, 0x00, 0x1f]));
-  });
 
-  it("Should convert 1970-02-01 to 0x460020", async () => {
-    const date = "1970-02-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x46, 0x00, 0x20]));
-  });
-
-  it("Should convert 1970-02-28 to 0x46003b", async () => {
-    const date = "1970-02-28";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x46, 0x00, 0x3b]));
-  });
-
-  it("Should convert 1970-10-01 to 0x460112", async () => {
-    const date = "1970-10-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x46, 0x01, 0x12]));
-  });
-
-  // BEGIN 2000 tests
-
-  it("Should throw an error, given 2000-01-32", async () => {
-    const date = "2000-01-32";
-    expect(() => getDateAsBytes(date)).to.throw();
-  });
-
-  it("Should convert 2000-01-01 to 0x640001", async () => {
-    const date = "2000-01-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x01]));
-  });
-
-  it("Should convert 2000-01-01 to 0x640001", async () => {
-    const date = "2000-01-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x01]));
-  });
-
-  it("Should convert 2000-01-02 to 0x640002", async () => {
-    const date = "2000-01-02";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x02]));
-  });
-
-  it("Should convert 2000-01-31 to 0x64001f", async () => {
-    const date = "2000-01-31";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x1f]));
-  });
-
-  it("Should convert 2000-02-01 to 0x640020", async () => {
-    const date = "2000-02-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x20]));
-  });
-
-  it("Should convert 2000-02-28 to 0x64003b", async () => {
-    const date = "2000-02-28";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x3b]));
-  });
-
-  it("Should convert 2000-02-29 to 0x64003c", async () => {
-    const date = "2000-02-29";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x00, 0x3c]));
-  });
-
-  it("Should convert 2000-10-01 to 0x640113", async () => {
-    const date = "2000-10-01";
-    const dateAsBytes = getDateAsBytes(date);
-    expect(dateAsBytes).to.deep.equal(Buffer.from([0x64, 0x01, 0x13]));
+  it("Should convert 2099-12-31 to 6311347200", async () => {
+    const date = "2099-12-31";
+    const dateAsInt = getDateAsInt(date);
+    expect(dateAsInt).to.equal(6311347200);
   });
 });
 
