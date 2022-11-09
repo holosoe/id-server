4f5e86e06a7bd90f7ea116c646f5d3050563b68a
calebtuttle <1calebtuttle@gmail.com>
2022-11-09 11:19:32 -0500

    Move close() commands to a new terminate function in server.js

--- src/main/server.js
+++ src/main/server.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { app } from "./index.js";
 import { sequelize } from "./init.js";
 
@@ -7,21 +8,27 @@ const server = app.listen(PORT, (err) => {
   console.log(`Server running, exposed at http://127.0.0.1:${PORT}`);
 });
 
-process.on("SIGTERM", async () => {
-  await sequelize.close();
-  console.log(`\nClosed SQL database connection`);
+async function terminate() {
+  try {
+    await sequelize.close();
+    console.log(`\nClosed SQL database connection`);
+  } catch (err) {
+    console.log(err);
+    console.log("An error occurred while attempting to close the SQL connection");
+  }
+  try {
+    await mongoose.connection.close();
+    console.log(`Closed MongoDB database connection`);
+  } catch (err) {
+    console.log(err);
+    console.log("An error occurred while attempting to close the MongoDB connection");
+  }
   console.log(`Closing server`);
   server.close(() => {
     console.log(`Closed server`);
     process.exit(0);
   });
-});
-process.on("SIGINT", async () => {
-  await sequelize.close();
-  console.log(`\nClosed SQL database connection`);
-  console.log(`Closing server`);
-  server.close(() => {
-    console.log(`Closed server`);
-    process.exit(0);
-  });
-});
+}
+
+process.on("SIGTERM", terminate);
+process.on("SIGINT", terminate);
