b4ba0ef70fb2a375473cb748dada0af7a7ca8cf9
calebtuttle <1calebtuttle@gmail.com>
2023-01-04 12:04:32 -0500

    Add dev phone issuer to holonymIssuers

--- src/constants/misc.js
+++ src/constants/misc.js
@@ -1,6 +1,7 @@
 export const holonymIssuers = [
   "0x8281316ac1d51c94f2de77575301cef615adea84", // gov-id
   "0xb625e69ab86db23c23682875ba10fbc8f8756d16", // phone
+  "0xfc8a8de489efefb91b42bb8b1a6014b71211a513", // phone dev
 ];
 export const relayerURL =
   process.env.NODE_ENV === "development"
