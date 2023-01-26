f37a031fba5057db15a0a3ee43ffdfcb4658f41a
Amos Elmaliah <amosel@gmail.com>
2023-01-26 11:41:34 +0200

    Update elsintrc preparing for typescript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {},
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: ["plugin:prettier/recommended"],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	parser: "@typescript-eslint/parser",
+	rules: {
+		"prettier/prettier": 2,
+	},
+	plugins: ["prettier"],
 };
