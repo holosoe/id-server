1c4240a1b792700e400b578b40c893b12419ffe7
Amos Elmaliah <amosel@gmail.com>
2023-01-26 11:45:28 +0200

    Added TSFixMe aliases supporting ts-migrate

---
+++ src/tsfixme.ts
@@ -0,0 +1,4 @@
+// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+type $TSFixMe = any;
+// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+type $TSFixMeFunction = (...args: any[]) => any;
