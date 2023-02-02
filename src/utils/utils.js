885eae832e01857ff9756ed81bae51408f0b8bcf
calebtuttle <1calebtuttle@gmail.com>
2023-02-02 09:22:47 -0500

    Remove old TODO

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -77,7 +77,6 @@ export async function createLeaf(
 
 export async function sendEmail(to, subject, text, html) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  // TODO: Setup SendGrid account, configure email in SendGrid, and test
   const msg = {
     to, // "test@example.com"
     from: "idservices@holonym.id",
