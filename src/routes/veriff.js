ee5d5a20547552b2f7be37eab4d1be2138eb4472
calebtuttle <1calebtuttle@gmail.com>
2023-08-09 14:41:35 -0400

    separate v1 and v2 veriff session creation endpoints

--- src/routes/veriff.js
+++ src/routes/veriff.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getCredentials } from "../services/veriff/credentials.js";
-import { createSession } from "../services/veriff/session.js";
+import { v1CreateSession, v2CreateSession } from "../services/veriff/session.js";
 import { decisionWebhook } from "../services/veriff/webhooks.js";
 
 const router = express.Router();
 
 router.get("/credentials", getCredentials);
-router.post("/session", createSession);
+router.post("/session", v1CreateSession);
+router.post("/v2/session", v2CreateSession);
 router.get("/decision-webhook", decisionWebhook);
 
 export default router;
