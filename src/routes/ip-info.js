4e9535393eff1d6d3b750e2fd05cc35f634bfff3
calebtuttle <1calebtuttle@gmail.com>
2023-08-14 10:03:56 -0400

    add ip-and-country endpoint

--- src/routes/ip-info.js
+++ src/routes/ip-info.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getCountry } from "../services/ip-info.js";
+import { getCountry, getIpAndCountry } from "../services/ip-info.js";
 
 const router = express.Router();
 
 router.get("/country", getCountry);
+router.get("/ip-and-country", getIpAndCountry);
 
 export default router;
--- src/services/ip-info.js
+++ src/services/ip-info.js
@@ -19,4 +19,28 @@ async function getCountry(req, res) {
   }
 }
 
-export { getCountry };
+async function getIpAndCountry(req, res) {
+  try {
+    const userIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    const resp = await axios.get(
+      `https://ipapi.co/${userIp}/json?key=${process.env.IPAPI_SECRET_KEY}`
+    );
+    const ip = resp?.data?.ip;
+    const country = resp?.data?.country_name;
+
+    return res.status(200).json({
+      ip,
+      country,
+    });
+  } catch (err) {
+    console.log("GET ip-info/ip-and-country: Error encountered (a)", err.message);
+    console.log(
+      "GET ip-info/ip-and-country: Error encountered (b)",
+      err?.response?.data
+    );
+    return res.status(500).json({ error: "An unknown error occurred" });
+  }
+}
+
+export { getCountry, getIpAndCountry };
