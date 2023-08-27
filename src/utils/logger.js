5a5187aa3486b86646828992b8de78c59b771dde
calebtuttle <1calebtuttle@gmail.com>
2023-08-27 16:27:50 -0400

    Send logs to datadog in production

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,10 +5,21 @@ const pinoOptions = {
 };
 
 if (process.env.NODE_ENV === "development") {
+  // Pretty print to console
+  pinoOptions.transport = { target: "pino-pretty", options: { colorize: true } };
+} else {
+  // Send logs to Datadog
   pinoOptions.transport = {
-    target: "pino-pretty",
+    target: "pino-datadog-transport",
     options: {
-      colorize: true,
+      ddClientConf: {
+        authMethods: {
+          apiKeyAuth: process.env.DATADOG_API_KEY,
+        },
+      },
+      ddServerConf: {
+        site: "us5.datadoghq.com",
+      },
     },
   };
 }
