04619b6ae3af6c8abd4107e238eefa3666dbbf76
calebtuttle <1calebtuttle@gmail.com>
2023-08-31 10:21:46 -0400

    Add createVeriffSession function

--- src/utils/veriff.js
+++ src/utils/veriff.js
@@ -1,5 +1,40 @@
 import { createHmac } from "crypto";
 import axios from "axios";
+import { v4 as uuidV4 } from "uuid";
+
+export async function createVeriffSession() {
+  try {
+    const frontendUrl =
+      process.NODE_ENV === "development"
+        ? "http://localhost:3002"
+        : "https://holonym.id";
+    const reqBody = {
+      verification: {
+        // TODO: Is callback necessary if we handle "FINISHED" event in frontend?
+        callback: `${frontendUrl}/mint`,
+        // document: {
+        //   type: "DRIVERS_LICENSE",
+        // },
+        vendorData: uuidV4(),
+        timestamp: new Date().toISOString(),
+      },
+    };
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "X-AUTH-CLIENT": process.env.VERIFF_PUBLIC_API_KEY,
+      },
+    };
+    const resp = await axios.post(
+      "https://api.veriff.me/v1/sessions",
+      reqBody,
+      config
+    );
+    return resp.data;
+  } catch (err) {
+    console.error("Error creating veriff session:", err.message, err.response?.data);
+  }
+}
 
 /**
  * @param {string} sessionId
