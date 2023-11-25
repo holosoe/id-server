f0e1ddd71e492ed4e6b815e83e8db0099015f468
calebtuttle <1calebtuttle@gmail.com>
2023-11-25 18:22:25 -0500

    add prices/v2 endpoint

--- src/routes/prices.js
+++ src/routes/prices.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPrice } from "../services/prices.js";
+import { getPrice, getPriceV2 } from "../services/prices.js";
 
 const router = express.Router();
 
 router.get("/", getPrice);
+router.get("/v2", getPriceV2);
 
 export default router;
--- src/services/prices.js
+++ src/services/prices.js
@@ -42,4 +42,50 @@ async function getPrice(req, res) {
   }
 }
 
-export { getPrice };
+async function getPriceV2(req, res) {
+  try {
+    const slug = req.query.slug;
+    if (!slug) {
+      return res.status(400).json({ error: "No slug provided." });
+    }
+
+    const slugs = slug.split(",");
+
+    const ids = [];
+    for (let i = 0; i < slugs.length; i++) {
+      const id = slugToID[slugs[i]];
+      ids.push(id);
+    }
+
+    const resp = await getLatestCryptoPrice(ids.join(","));
+
+    const price = {};
+
+    for (let i = 0; i < slugs.length; i++) {
+      const slug = slugs[i];
+      const id = slugToID[slug];
+      price[slug] = resp?.data?.data?.[id]?.quote?.USD?.price;
+    }
+
+    return res.status(200).json({
+      price,
+    });
+  } catch (err) {
+    if (err.response) {
+      endpointLogger.error(
+        { error: err.response.data },
+        "Error getting price from CMC"
+      );
+    } else if (err.request) {
+      endpointLogger.error(
+        { error: err.request.data },
+        "Error getting price from CMC"
+      );
+    } else {
+      endpointLogger.error({ error: err.message }, "Error getting price from CMC");
+    }
+    return res.status(500).json({ error: "An unknown error occurred" });
+  }
+}
+
+export { getPrice, getPriceV2 };
