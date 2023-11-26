544b38ef19d7da5e531e02bbf8b6714573198bc0
calebtuttle <1calebtuttle@gmail.com>
2023-11-26 09:22:13 -0500

    prices/v2: Implement simple cache mechanism

--- src/services/prices.js
+++ src/services/prices.js
@@ -9,6 +9,10 @@ const endpointLogger = logger.child({
   },
 });
 
+// TODO: Use redis instead. This is a temporary solution to avoid hitting
+// CMC's rate limit. key-value pair is { slug: { price: number, lastUpdatedAt: Date } }
+const cryptoPrices = {};
+
 async function getPrice(req, res) {
   try {
     const slug = req.query.slug;
@@ -51,23 +55,52 @@ async function getPriceV2(req, res) {
 
     const slugs = slug.split(",");
 
+    // Check cache first
+    const cachedPrices = {};
+    const now = new Date();
+    for (let i = 0; i < slugs.length; i++) {
+      const slug = slugs[i];
+      const cachedPrice = cryptoPrices[slug];
+      // If price was last updated less than 30 seconds ago, use cached price
+      if (cachedPrice && now - cachedPrice.lastUpdatedAt < 30 * 1000) {
+        cachedPrices[slug] = cachedPrice.price;
+      }
+    }
+
+    // Get ids of cryptos whose prices weren't retrieved from cache
     const ids = [];
     for (let i = 0; i < slugs.length; i++) {
-      const id = slugToID[slugs[i]];
-      ids.push(id);
+      const slug = slugs[i];
+      if (!cachedPrices[slug]) {
+        ids.push(slugToID[slug]);
+      }
+    }
+
+    if (ids.length === 0) {
+      return res.status(200).json(cachedPrices);
     }
 
     const resp = await getLatestCryptoPrice(ids.join(","));
 
-    const price = {};
+    const newPrices = {};
 
     for (let i = 0; i < slugs.length; i++) {
       const slug = slugs[i];
+
+      // Ignore slugs whose prices were retrieved from cache
+      if (cachedPrices[slug]) continue;
+
       const id = slugToID[slug];
-      price[slug] = resp?.data?.data?.[id]?.quote?.USD?.price;
+      newPrices[slug] = resp?.data?.data?.[id]?.quote?.USD?.price;
+
+      // Update cache
+      cryptoPrices[slug] = {
+        price: newPrices[slug],
+        lastUpdatedAt: new Date(),
+      };
     }
 
-    return res.status(200).json(price);
+    return res.status(200).json({ ...newPrices, ...cachedPrices });
   } catch (err) {
     if (err.response) {
       endpointLogger.error(
