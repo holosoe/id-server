02396b2f4f73d1ebf1b968f4df9362f5afaac30b
calebtuttle <1calebtuttle@gmail.com>
2023-12-21 12:56:57 -0500

    add admin/fail-session endpoint

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,6 +7,7 @@ import { deleteUserData } from "../services/admin/user-idv-data.js";
 import { transferFunds } from "../services/admin/transfer-funds.js";
 import { setSessionIdvProvider } from "../services/admin/set-session-idv-provider.js";
 import { userSessions } from "../services/admin/user-sessions.js";
+import { failSession } from "../services/admin/fail-session.js";
 
 const router = express.Router();
 
@@ -16,5 +17,6 @@ router.delete("/user-idv-data", deleteUserData);
 router.post("/transfer-funds", transferFunds);
 router.post("/set-session-idv-provider", setSessionIdvProvider);
 router.post("/user-sessions", userSessions);
+router.post("/fail-session", failSession);
 
 export default router;
---
+++ src/services/admin/fail-session.js
@@ -0,0 +1,59 @@
+import { ObjectId } from "mongodb";
+import { Session } from "../../init.js";
+import { sessionStatusEnum } from "../../constants/misc.js";
+import logger from "../../utils/logger.js";
+
+const postEndpointLogger = logger.child({
+  msgPrefix: "[POST /admin/fail-session] ",
+});
+
+/**
+ * Admin endpoint for manually failing a session. This allows a user
+ * to request a refund AFTER paying but BEFORE failing verification.
+ */
+async function failSession(req, res) {
+  try {
+    const apiKey = req.headers["x-api-key"];
+
+    if (apiKey !== process.env.ADMIN_API_KEY_LOW_PRIVILEGE) {
+      return res.status(401).json({ error: "Invalid API key." });
+    }
+
+    const id = req.body.id;
+
+    if (!id) {
+      return res.status(400).json({ error: "No user ID specified." });
+    }
+
+    let objectId = null;
+    try {
+      objectId = new ObjectId(id);
+    } catch (err) {
+      return res.status(400).json({ error: "Invalid _id" });
+    }
+
+    const session = await Session.findOne({ _id: objectId }).exec();
+
+    if (!session) {
+      return res.status(404).json({ error: "Session not found" });
+    }
+
+    if (session.status !== sessionStatusEnum.IN_PROGRESS) {
+      return res.status(400).json({
+        error: `Session status is ${session.status}. Must be one ${sessionStatusEnum.IN_PROGRESS}`,
+      });
+    }
+
+    session.status = sessionStatusEnum.VERIFICATION_FAILED;
+    await session.save();
+
+    return res.status(200).json({
+      message: `Changed status of session ${id} to ${sessionStatusEnum.VERIFICATION_FAILED}`,
+    });
+  } catch (err) {
+    postEndpointLogger.error({ error: err });
+    return res.status(500).json({ error: "An unknown error occurred" });
+  }
+}
+
+export { failSession };
