b7c4a281af5d2119943cc67ca52255bb07e9f2f0
Caleb Tuttle <1calebtuttle@gmail.com>
2024-05-06 22:02:32 -0400

    fix

--- src/services/admin/user-verifications.js
+++ src/services/admin/user-verifications.js
@@ -82,7 +82,7 @@ async function deleteUserVerification(req, res) {
       date: new Date().toISOString().slice(0, 10),
     }).exec();
     const deletionCountToday = deletionCountDoc?.deletionCount ?? 0;
-    if (deletionCountToday >= sessionCountToday * 0.02 + 30) {
+    if (deletionCountToday >= sessionCountToday * 0.02) {
       deleteEndpointLogger.info("Deletion limit reached for today. Exiting.");
       return res
         .status(429)
