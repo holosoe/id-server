1e15049b2d4862c2a33075a6133913ae1126719a
calebtuttle <1calebtuttle@gmail.com>
2024-05-14 09:58:56 -0400

    rename "veriff" files to "verif-kyc"

--- src/constants/cmc.js
+++ src/constants/cmc.js
@@ -1,9 +1,11 @@
 export const ethereumCMCID = 1027;
 export const avalancheCMCID = 5805;
 export const fantomCMCID = 3513;
+export const nearCMCID = 6535;
 
 export const slugToID = {
   ethereum: ethereumCMCID,
   avalanche: avalancheCMCID,
   fantom: fantomCMCID,
+  near: nearCMCID,
 };
