41ee96eb931aa34b0a3f4fa02be670819c9f4651
calebtuttle <1calebtuttle@gmail.com>
2024-05-15 11:10:04 -0400

    add getVeriffSessionWatchlistScreening

--- src/utils/veriff.js
+++ src/utils/veriff.js
@@ -112,3 +112,32 @@ export async function deleteVeriffSession(sessionId) {
     console.error("Error deleting veriff session:", err.message, err.response?.data);
   }
 }
+
+/**
+ * @param {string} sessionId
+ */
+export async function getVeriffSessionWatchlistScreening(sessionId) {
+  try {
+    const hmacSignature = createHmac("sha256", process.env.VERIFF_SECRET_API_KEY)
+      .update(Buffer.from(sessionId, "utf8"))
+      .digest("hex")
+      .toLowerCase();
+    const resp = await axios.get(
+      `https://api.veriff.me/v1/sessions/${sessionId}/watchlist-screening`,
+      {
+        headers: {
+          "X-AUTH-CLIENT": process.env.VERIFF_PUBLIC_API_KEY,
+          "X-HMAC-SIGNATURE": hmacSignature,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return resp.data;
+  } catch (err) {
+    console.error(
+      "Error getting veriff session decision:",
+      err.message,
+      err.response?.data
+    );
+  }
+}
