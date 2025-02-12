dd434e60e0866bf7cefd29ce273241ae5d482e3e
Nanak Nihal Khalsa <nanaknihal@gmail.com>
2025-02-12 03:16:34 -0800

    fix cmc slug for fantom

--- src/constants/cmc.js
+++ src/constants/cmc.js
@@ -1,6 +1,7 @@
 export const ethereumCMCID = 1027;
 export const avalancheCMCID = 5805;
-export const fantomCMCID = 3513;
+// export const fantomCMCID = 3513;
+export const fantomCMCID = 32684; // Sonic
 export const nearCMCID = 6535;
 
 export const slugToID = {
