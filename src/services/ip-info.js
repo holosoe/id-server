cf85cc03495f77e2b3156a8bd8e543a0a52b962c
Caleb Tuttle <1calebtuttle@gmail.com>
2025-02-13 09:20:32 -0500

    UNDO THIS CHANGE ONCE WE FIX RATE LIMIT FROM IPAPI. getCountry: always return "United States"

--- src/services/ip-info.js
+++ src/services/ip-info.js
@@ -2,16 +2,21 @@ import axios from "axios";
 
 async function getCountry(req, res) {
   try {
-    const userIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-
-    const resp = await axios.get(
-      `https://ipapi.co/${userIp}/json?key=${process.env.IPAPI_SECRET_KEY}`
-    );
-    const country = resp?.data?.country_name;
-
+    // Feb 13, 2025: Getting rate limited by ipapi. So we just pretend all users are from US for now
     return res.status(200).json({
-      country,
+      country: 'United States'
     });
+
+    // const userIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    // const resp = await axios.get(
+    //   `https://ipapi.co/${userIp}/json?key=${process.env.IPAPI_SECRET_KEY}`
+    // );
+    // const country = resp?.data?.country_name;
+
+    // return res.status(200).json({
+    //   country,
+    // });
   } catch (err) {
     console.log("GET ip-info/country: Error encountered (a)", err.message);
     console.log("GET ip-info/country: Error encountered (b)", err?.response?.data);
