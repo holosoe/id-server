6277e896178d58e3bbba34af2e17383870cac09f
Caleb Tuttle <1calebtuttle@gmail.com>
2025-02-14 19:29:25 -0500

    asdf

--- transaction-monitor/src/index.ts
+++ transaction-monitor/src/index.ts
@@ -407,7 +407,7 @@ async function processPhoneServerTransactions() {
       try {
         digest = ethers.utils.keccak256("0x" + session.id);
       } catch (err) {
-        console.error(err)
+        console.error('error hashing session id', (err as any).message)
         continue;
       }
 
